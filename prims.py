0c94012a44ba77cff9141b0836718e14b493dacb
Mahesh Gaikwad <gaikwadmahesh777g@gmail.com>
2023-05-16 10:54:07 +0530

    updated

--- prims.py
+++ prims.py
@@ -1,88 +1,45 @@
-# A Python3 program for
-# Prim's Minimum Spanning Tree (MST) algorithm.
-# The program is for adjacency matrix
-# representation of the graph
-
-# Library for INT_MAX
-import sys
+# Prim's Algorithm in Python
 
 
-class Graph():
-	def _init_(self, vertices):
-		self.V = vertices
-		self.graph = [[0 for column in range(vertices)]
-					for row in range(vertices)]
-
-	# A utility function to print
-	# the constructed MST stored in parent[]
-def printMST(self, parent):
-    print("Edge \tWeight")
-    for i in range(1, self.V):
-        print(parent[i], "-", i, "\t", self.graph[i][parent[i]])
-
-# A utility function to find the vertex with
-# minimum distance value, from the set of vertices
-# not yet included in shortest path tree
-def minKey(self, key, mstSet):
-
-    # Initialize min value
-    min = sys.maxsize
-
-    for v in range(self.V):
-        if key[v] < min and mstSet[v] == False:
-            min = key[v]
-            min_index = v
-
-    return min_index
-
-	# Function to construct and print MST for a graph
-	# represented using adjacency matrix representation
-def primMST(self):
-
-    # Key values used to pick minimum weight edge in cut
-    key = [sys.maxsize] * self.V
-    parent = [None] * self.V # Array to store constructed MST
-    # Make key 0 so that this vertex is picked as first vertex
-    key[0] = 0
-    mstSet = [False] * self.V
-
-    parent[0] = -1 # First node is always the root of
-
-    for cout in range(self.V):
-
-        # Pick the minimum distance vertex from
-        # the set of vertices not yet processed.
-        # u is always equal to src in first iteration
-        u = self.minKey(key, mstSet)
-
-        # Put the minimum distance vertex in
-        # the shortest path tree
-        mstSet[u] = True
-
-        # Update dist value of the adjacent vertices
-        # of the picked vertex only if the current
-        # distance is greater than new distance and
-        # the vertex in not in the shortest path tree
-        for v in range(self.V):
-
-            # graph[u][v] is non zero only for adjacent vertices of m
-            # mstSet[v] is false for vertices not yet included in MST
-            # Update the key only if graph[u][v] is smaller than key[v]
-            if self.graph[u][v] > 0 and mstSet[v] == False \
-            and key[v] > self.graph[u][v]:
-                key[v] = self.graph[u][v]
-                parent[v] = u
-
-    self.printMST(parent)
-
-
-# Driver's code
-if __name__ == '_main_':
-	g = Graph(5)
-	g.graph = [[0, 2, 0, 6, 0],
-			[2, 0, 3, 8, 5],
-			[0, 3, 0, 0, 7],
-			[6, 8, 0, 0, 9],
-			[0, 5, 7, 9, 0]]
-
-	g.primMST()
+INF = 9999999
+# number of vertices in graph
+V = 5
+# create a 2d array of size 5x5
+# for adjacency matrix to represent graph
+G = [[0, 9, 75, 0, 0],
+     [9, 0, 95, 19, 42],
+     [75, 95, 0, 51, 66],
+     [0, 19, 51, 0, 31],
+     [0, 42, 66, 31, 0]]
+# create a array to track selected vertex
+# selected will become true otherwise false
+selected = [0, 0, 0, 0, 0]
+# set number of edge to 0
+no_edge = 0
+# the number of egde in minimum spanning tree will be
+# always less than(V - 1), where V is number of vertices in
+# graph
+# choose 0th vertex and make it true
+selected[0] = True
+# print for edge and weight
+print("Edge : Weight\n")
+while (no_edge < V - 1):
+    # For every vertex in the set S, find the all adjacent vertices
+    #, calculate the distance from the vertex selected at step 1.
+    # if the vertex is already in the set S, discard it otherwise
+    # choose another vertex nearest to selected vertex  at step 1.
+    minimum = INF
+    x = 0
+    y = 0
+    for i in range(V):
+        if selected[i]:
+            for j in range(V):
+                if ((not selected[j]) and G[i][j]):  
+                    # not in selected and there is an edge
+                    if minimum > G[i][j]:
+                        minimum = G[i][j]
+                        x = i
+                        y = j
+    print(str(x) + "-" + str(y) + ":" + str(G[x][y]))
+    selected[y] = True
+    no_edge += 1
